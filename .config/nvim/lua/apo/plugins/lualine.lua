f31af53807be900251f93dc6f6b3810512b14bde
Ankit Pokhrel <oss@ankit.pl>
2024-12-01 12:12:59 +0100

    Update lualine

--- .config/nvim/lua/apo/plugins/lualine.lua
+++ .config/nvim/lua/apo/plugins/lualine.lua
@@ -2,6 +2,18 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require('lualine').setup()
+        require('lualine').setup({
+            options = {
+                theme = 'auto',
+            },
+            sections = {
+                lualine_b = {
+                  {'branch', color = {fg = '#f8fbf8'}}, -- Git branch
+                },
+                lualine_y = {
+                  {'progress', color = {fg = '#f8fbf8'}}, -- Scroll percent
+                },
+            }
+        })
     end
 }
